6984e98ff32034aa7aed6de468f5e4f1070d646a
GabrielFNLima <gabrielfnlima@gmail.com>
2022-09-02 01:09:55 +0000

    Tentativa getList e alterações

--- Model/ResourceModel/Example/Collection.php
+++ Model/ResourceModel/Example/Collection.php
@@ -2,7 +2,14 @@
 
 namespace GFNL\ModelExample\Model\ResourceModel\Example;
 
-class Colection
-{
+use GFNL\ModelExample\Model\Example;
+use GFNL\ModelExample\Model\ResourceModel\Example as ResourceModelExample;
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
+class Collection extends AbstractCollection
+{
+    protected function _construct()
+    {
+        $this->_init(Example::class, ResourceModelExample::class);
+    }
 }
