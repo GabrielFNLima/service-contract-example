0b493daba225bd2438fa11535aba442c047d056c
GabrielFNLima <gabrielfnlima@gmail.com>
2022-09-03 01:36:53 +0000

    deleteById controlller

---
+++ Controller/Index/DeleteById.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace GFNL\ModelExample\Controller\Index;
+
+use \Magento\Framework\Controller\Result\RawFactory;
+
+class DeleteById extends \Magento\Framework\App\Action\Action
+{
+
+    /**
+     * Index resultPageFactory
+     * @var PageFactory
+     */
+    private $resultPageFactory;
+    /**
+     * @var
+     */
+    private $modelRepository;
+
+    /**
+     * Index constructor.
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \GFNL\ModelExample\Model\ExampleRepository $modelRepository
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \GFNL\ModelExample\Model\ExampleRepository $modelRepository,
+        \Magento\Framework\App\Request\Http $request
+
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->modelRepository = $modelRepository;
+        $this->request = $request;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        echo $this->modelRepository->deleteById($this->request->getParam('id'));
+        $this->resultPageFactory->create("raw");
+    }
+}
